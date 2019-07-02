57deca96695bc0353e57271659078eecae035089
Ubuntu <ubuntu@dev29juqu17.novalocal>
2019-07-02 13:10:23 -0400

    martes2julio

---
+++ primerasemana/tipodatos.cpp
@@ -0,0 +1,45 @@
+//bookl (true,false)
+//auto pongan variable en lugar de su contexto 
+//compilador deduxzaca el tipo de variable, al momentod e crear la variable 
+//char = 1bite
+//int 
+#include<iostream>
+using namespace std;
+int main()
+{
+    //bool p=45;
+    //auto p = true;
+    //cout<<p<<"\n";
+
+    /* char h= 31;
+    char s= 18;
+    char p= h+s;
+
+    cout<<(int)p <<"\n";
+
+
+    char s = 'd';
+     s++;
+     cout<<s<<"\n";
+
+     int a = 458;
+     int b = 485692831;
+     int c = 48'569'283;
+     int d = 0xcafecaf9;
+     int e = 0107012;
+
+     auto s = 2'000'000'000U;//auto
+     auto r = 2'000'000'000U;
+     
+     auto r2= s+r;
+     cout<<r2<<"\n";*/
+
+
+     //short
+
+     short n = 25;
+     short n2 = (short)6484257;
+
+     signed short q = 24; 
+}
+
